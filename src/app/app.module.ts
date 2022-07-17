4963cbc26e336f4e05f0233509911736707df206
hycheng5 <hycheng5@winbond.com>
2022-07-17 09:58:07 +0800

    設定基本路由規則+啟用 HashLocationStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    DashboardComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
