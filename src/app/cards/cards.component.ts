0081250a1889ec8afec939d6bc23795bc2596632
hycheng5 <hycheng5@winbond.com>
2022-07-17 16:09:40 +0800

    add paramMap and queryParamMap

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 @Component({
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
 })
 export class CardsComponent implements OnInit {
+  constructor(private router: Router, private routr: ActivatedRoute) {}
 
-  constructor() { }
+  type: string | null = '';
+  name: string | null = '';
 
   ngOnInit(): void {
+    this.routr.paramMap.subscribe((params: ParamMap) => {
+      this.type = params.get('type');
+    });
+
+    this.routr.queryParamMap.subscribe((params: ParamMap) => {
+      this.name = params.get('name');
+    });
   }
 
+  inc(num: number) {
+    this.router.navigate(['../', +(this.type || 0) + num], {
+      queryParamsHandling: 'preserve',
+      relativeTo: this.routr,
+    });
+  }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { chartAreaDemo } from '../chartAreaDemo';
 import { chartPieDemo } from '../chartPieDemo';
 
@@ -7,10 +8,22 @@ import { chartPieDemo } from '../chartPieDemo';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     chartAreaDemo();
     chartPieDemo();
+  }
+
+  gotoCards(type: number, name: string) {
+    this.router.navigateByUrl('/cards/' + type + '?name=' + name);
+  }
+
+  gotoCards2(type: number, name: string) {
+    this.router.navigate(['/cards/123'], {
+      queryParams: {
+        name: name,
+      },
+    });
   }
 }
